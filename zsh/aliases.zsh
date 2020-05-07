ab40a2a1a3664b42b2d4504d6f2afdf3bcc262ae
Dennis Bakhuis <dennis.bakhuis@ecare.nl>
2020-05-07 17:04:50 +0200

    added tmux, changed zsh/vimrc

---
+++ zsh/aliases.zsh
@@ -0,0 +1,32 @@
+# My aliases
+
+###########
+# General #
+###########
+alias ls='ls --color=auto'
+
+#######
+# Vim #
+#######
+alias v='vim'
+alias ez='vim ~/dotfiles/zsh/zshrc.default'
+alias ea='vim ~/dotfiles/zsh/aliases.zsh'
+
+
+#######
+# Git #
+#######
+alias g='git'
+alias ga='git add'
+alias gaa='git add --all'
+alias gc='git commit -v'
+alias gs='git status'
+alias gr='git reset'
+
+#########
+# Conda #
+#########
+alias ca='conda activate'
+
+
+
