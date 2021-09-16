4c9263239d1bf6a12262723c5df539679ab6b439
Dennis Bakhuis <dennis@bakhuis.nu>
2021-09-16 15:32:18 +0200

    new install script

---
+++ install_manjaro.sh
@@ -0,0 +1,67 @@
+#!/bin/zsh
+# General install file of my setup
+# Expects zsh to be installed
+
+####################
+# Helper functions #
+####################
+
+# Returns whether the given command is executable or aliased.
+_has() {
+  return $( whence $1 >/dev/null )
+}
+
+_nextfilename() {
+  name=$1
+  if [[ -e $name || -L $name ]] ; then
+    i=1
+    while [[ -e $name.$i || -L $namer.$i ]] ; do
+        let i++
+    done
+    name=$name.$i
+  fi
+  echo $name
+}
+
+########
+# Main #
+########
+
+# create directories if not exist
+mkdir -p $HOME/.cache/vim
+mkdir -p $HOME/.cache/zsh
+mkdir -p $HOME/.config/alacritty
+mkdir -p $HOME/.config/nvim
+
+rm -Rf $HOME/.zplug
+
+# required packages
+sudo pacman -S \
+  zsh curl gawk fzf the_silver_searcher \
+  adobe-source-code-pro-fonts alacritty
+
+# install zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+# symlinks
+# cp $HOME/.zshrc $(_nextfilename $HOME/zshrc.old)
+rm -f $HOME/.zshrc
+ln -s $HOME/dotfiles/zsh/zshrc.ecare $HOME/.zshrc
+
+# cp $HOME/.vimrc $(_nextfilename $HOME/vimrc.old) # probably do not need backup
+rm -f $HOME/.vimrc
+ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
+
+rm -f $HOME/.config/nvim/init.vim
+ln -s $HOME/dotfiles/vim/vimrc $HOME/.config/nvim/init.vim
+
+rm -f $HOME/.alacritty.yml
+ln -s $HOME/dotfiles/alacritty/alacritty.yml.ecare $HOME/.alacritty.yml
+
+rm -f $HOME/.tmux.conf
+ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+
+if _has conda; then
+  conda config --set changeps1 False
+fi
+
