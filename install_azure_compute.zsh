00e54319d17ac1bbab952f4fe7d9f8d5f976a4cf
Dennis Bakhuis <dennis@bakhuis.nu>
2022-08-31 10:07:59 +0000

    install_azure_cloud.zsh script added

--- install_azure_compute.zsh
+++ install_azure_compute.zsh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 ####################
 # Helper functions #
 ####################
@@ -19,14 +21,17 @@ mkdir -pv $HOME/.cache/zsh
 mkdir -pv $HOME/.config/nvim
 
 # install additional packages
-sudo apt install -y zsh zplug tmux neovim curl gawk fzf silversearcher-ag nodejs meld
+sudo apt install -y zsh zplug tmux neovim curl gawk fzf silversearcher-ag nodejs meld lua5.3
 
 # make zsh shell
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $(whoami)
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # symlink all config files
 rm -f $HOME/.zshrc
-ln -s $HOME/dotfiles/zsh/zshrc.tennet $HOME/.zshrc
+ln -s $HOME/dotfiles/zsh/zshrc.azurevm $HOME/.zshrc
 
 rm -f $HOME/.config/nvim/init.vim
 ln -s $HOME/dotfiles/vim/vimrc $HOME/.config/nvim/init.vim
