d655a37cc61c70abb51053f0c87d35e68960969a
Dennis Bakhuis <dennis@bakhuis.nu>
2022-08-31 10:20:21 +0000

    added poetry to install list

--- install_azure_compute.zsh
+++ install_azure_compute.zsh
@@ -26,8 +26,8 @@ sudo apt install -y zsh zplug tmux neovim curl gawk fzf silversearcher-ag nodejs
 # make zsh shell
 sudo chsh -s $(which zsh) $(whoami)
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # symlink all config files
 rm -f $HOME/.zshrc
@@ -39,6 +39,13 @@ ln -s $HOME/dotfiles/vim/vimrc $HOME/.config/nvim/init.vim
 rm -f $HOME/.tmux.conf
 ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 
+# install vim environment for vim Python plugins
+conda create -n vim python=3.10 neovim black
+
+# do a vimplug install
+nvim +'PlugInstall --sync' +qa
+nvim +'UpdateRemotePlugins --sync' +qa
+
 # remove env prompt in conda
 if _has conda; then
   conda config --set changeps1 False
@@ -46,4 +53,7 @@ fi
 
 # Use meld as diff tool in git
 git config --global diff.tool meld
+
+# Install Poetry
+curl -sSL https://install.python-poetry.org | python3 -
 
