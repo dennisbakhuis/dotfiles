ed76565adbae27c5801ecf97a25ae27cc09c806a
Dennis Bakhuis <dennis@bakhuis.nu>
2022-09-01 09:07:13 +0000

    added rsa_id key scripts

---
+++ azure_scripts/download_rsa_id.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from azureml.core import Workspace
+
+ws = Workspace.from_config()
+kv = ws.get_default_keyvault()
+
+secret_name_rsaid_private = 'tennet-compute-rsaid-private-key-dennis'
+secret_name_rsaid_public = 'tennet-compute-rsaid-public-key-dennis'
+
+private_key_path = Path('/home/azureuser/.ssh/id_rsa')
+if not private_key_path.exists():
+    with open(private_key_path) as f:
+        private_key = kv.get_secret(
+            secret_name_rsaid_private,
+        )
+        f.write(private_key)
+
+public_key_path = Path('/home/azureuser/.ssh/id_rsa.pub')
+if not public_key_path.exists():
+    with open(public_key_path) as f:
+        public_key = kv.get_secret(
+            secret_name_rsaid_public,
+        )
+        f.write(public_key)
+
--- install_azure_compute.sh
+++ install_azure_compute.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-
-####################
-# Helper functions #
-####################
-
-# Returns whether the given command is executable or aliased.
-_has() {
-  return $( whence $1 >/dev/null )
-}
-
-########
-# Main #
-########
+###############################
+# AzureML Compute prep script #
+###############################
+# Author: Dennis Bakhuis      #
+# Date: 2022-09-01            #
+###############################
 
 # Update current system
 sudo apt-get update && sudo apt-get -y upgrade
@@ -42,18 +35,10 @@ rm -f $HOME/.tmux.conf
 ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 
 # install vim environment for vim Python plugins
-conda create --name vim --channel conda-forge --force --yes python=3.10
-conda activate vim
-pip install neovim black
-conda deactivate
+conda create --name vim --channel conda-forge --force --yes python=3.9 neovim black
 
 # do a vimplug install
 nvim +'PlugInstall --sync' +'UpdateRemotePlugins' +qa
-
-# remove env prompt in conda
-if _has conda; then
-  conda config --set changeps1 False
-fi
 
 # Use meld as diff tool in git
 git config --global diff.tool meld
@@ -63,8 +48,9 @@ curl -sSL https://install.python-poetry.org | python3 -
 path+=('/home/azureuser/.local/bin')
 poetry completions zsh > ~/.zfunc/_poetry
 
-# Add private repo
+# Add TenneT private repo
 export PAT=$(ipython -c "from azureml.core import Workspace;ws=Workspace.from_config();kv=ws.get_default_keyvault();print(kv.get_secret('private-pypi-repo-pat-token-dennis'))")
 echo "[backend]\ndefault-keyring=keyring.backends.fail.Keyring\n" > $HOME/.config/python_keyring/keyringrc.cfg
 poetry config http-basic.tennet build "$PAT"
+
 
