eda94461d384104325507dd6b1e6de867b95db1c
Dennis Bakhuis <git@bakhuis.nu>
2024-02-14 23:55:19 +0100

    playing with colorschemes

--- nvim/lua/bakhuis/plugins/colorscheme.lua
+++ nvim/lua/bakhuis/plugins/colorscheme.lua
@@ -8,3 +8,15 @@ return {
         vim.cmd([[highlight WinSeparator guibg=None ]])
 	end,
 }
+-- return {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     priority = 1000,
+-- 	config = function()
+-- 		vim.cmd([[colorscheme catppuccin]])
+--
+--         -- Make Window separator bg transparent
+--         vim.cmd([[highlight WinSeparator guibg=None ]])
+-- 	end,
+-- }
+
