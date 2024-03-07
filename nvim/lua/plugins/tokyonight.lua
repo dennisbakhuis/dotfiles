b98724d409fc792769b06ec7ecc3e0af591270d1
Dennis Bakhuis <git@bakhuis.nu>
2024-03-07 07:15:29 +0100

    brought back transparency

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -3,20 +3,14 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       transparent = true,
-      -- styles = {
-      --   sidebars = "transparent",
-      --   floats = "transparent",
-      -- },
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_colors = function(colors)
+        colors.border = "#565f89"
+      end,
     },
-    config = function()
-      vim.g.tokyonight_style = "night"
-      require("tokyonight").setup({
-        -- other configs
-        on_colors = function(colors)
-          colors.border = "#565f89"
-        end,
-      })
-    end,
   },
   {
     "LazyVim/LazyVim",
